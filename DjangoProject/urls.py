56c5d18a24b9582dc5151d46db8205d4f66752fb
desertiociane <desertiocaine@gmail.com>
2025-07-02 15:09:15 -0500

    Password reset functionality

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
 from django.conf import settings
 from django.contrib.auth.views import LoginView
@@ -24,8 +25,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('', include('Escape_room.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
-    settings.MEDIA_ROOT}), #serve media files when deployed
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
-    settings.STATIC_ROOT}), #serve static files when deployed
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}), #serve media files when deployed
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
